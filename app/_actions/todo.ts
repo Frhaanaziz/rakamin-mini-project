e62ebe9d383d27df12ae532bf6f313387b4dfff3
Farhan <farhan@aththariq.com>
2024-05-09 09:21:06 +0700

    refactor: Update createTodo function in todo actions

--- app/_actions/todo.ts
+++ app/_actions/todo.ts
@@ -3,30 +3,21 @@
 import { backendApi } from '@/lib/axios';
 import { getErrorMessage } from '@/lib/utils';
 import { createTodoSchema } from '@/lib/validators/todo';
+import { Todo } from '@/types';
 import { createSafeActionClient } from 'next-safe-action';
 import { revalidatePath } from 'next/cache';
 
 export const action = createSafeActionClient();
 
 export const createTodo = action(createTodoSchema, async (values) => {
-  try {
-    const { data } = await backendApi.post('/todos', values);
+  const { data } = await backendApi.post('/todos', values);
 
-    revalidatePath('/');
-    return { error: null, data };
-  } catch (error) {
-    console.error('Error creating todo:', error);
-    return { error: getErrorMessage(error), data: null };
-  }
+  revalidatePath('/');
+  return data as Todo;
 });
 
 export const getTodos = async () => {
-  try {
-    const { data } = await backendApi.get('/todos');
+  const { data } = await backendApi.get('/todos');
 
-    return { error: null, data };
-  } catch (error) {
-    console.error('Error getting todos:', error);
-    return { error: getErrorMessage(error), data: null };
-  }
+  return data;
 };
