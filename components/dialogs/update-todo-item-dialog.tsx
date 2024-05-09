9a00520ae30d8938c56d287860d9e2fdf3a2ce4c
Farhan <farhan@aththariq.com>
2024-05-09 23:57:14 +0700

    feat: Add UpdateTodoItemDialog component for updating todo items

---
+++ components/dialogs/update-todo-item-dialog.tsx
@@ -0,0 +1,45 @@
+'use client';
+import React from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import UpdateTodoItemForm from '../forms/update-todo-item-form';
+import { Item } from '@/types';
+
+interface UpdateTodoItemDialogProps {
+  todoId: number;
+  todoItem: Item;
+  dialogTrigger: JSX.Element;
+}
+
+const UpdateTodoItemDialog = ({
+  todoId,
+  todoItem,
+  dialogTrigger,
+}: UpdateTodoItemDialogProps) => {
+  const [isDialogOpen, setIsDialogOpen] = React.useState<boolean>(false);
+
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>{dialogTrigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Task</DialogTitle>
+        </DialogHeader>
+
+        <UpdateTodoItemForm
+          className="mt-3"
+          setIsDialogOpen={setIsDialogOpen}
+          todoId={todoId}
+          todoItem={todoItem}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default UpdateTodoItemDialog;
