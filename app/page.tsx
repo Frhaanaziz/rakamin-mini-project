49eda5bb1633c172db248a6f44aa48a1279a3e44
Farhan <farhan@aththariq.com>
2024-05-09 23:58:21 +0700

    refactor: Update Home page to fetch and display todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <main></main>;
+import Todo from '@/components/todo';
+import { getTodos } from './_actions/todo';
+
+export default async function Home() {
+  const { data: todos } = await getTodos(undefined);
+  if (!todos) throw new Error('No todos found');
+
+  return (
+    <main className="container my-6">
+      {/* <KanbanBoard /> */}
+      <section className="grid grid-cols-4 gap-4">
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <Todo todo={todo} />
+          </div>
+        ))}
+      </section>
+    </main>
+  );
 }
