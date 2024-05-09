079481e998db0ca4be07fa724c20ee14a549c779
Farhan <farhan@aththariq.com>
2024-05-09 09:00:24 +0700

    feat: Add Toaster component for UI notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito_Sans as FontSans } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import './globals.css';
 import Header from '@/components/layouts/Header';
+import { Toaster } from '@/components/ui/sonner';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -28,6 +29,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
+        <Toaster position="top-center" expand={false} richColors />
         <Header />
         {children}
       </body>
