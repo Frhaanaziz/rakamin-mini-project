7ca7019346211b7ad31ce6dbc4177ce8d50137bd
Farhan <farhan@aththariq.com>
2024-05-09 23:57:25 +0700

    feat: Add CreateTodoItemDialog component for creating todo items

---
+++ components/dialogs/create-todo-item-dialog.tsx
@@ -0,0 +1,41 @@
+'use client';
+import React from 'react';
+import CreateTodoForm from '../forms/create-todo-form';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { PlusCircleIcon } from 'lucide-react';
+import CreateTodoItemForm from '../forms/create-todo-item-form';
+
+interface CreateTodoItemDialogProps {
+  todo_id: number;
+}
+
+const CreateTodoItemDialog = ({ todo_id }: CreateTodoItemDialogProps) => {
+  const [isDialogOpen, setIsDialogOpen] = React.useState<boolean>(false);
+
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger className="flex items-center gap-2 text-xs mt-2.5">
+        <PlusCircleIcon size={18} /> New Task
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Task</DialogTitle>
+        </DialogHeader>
+
+        <CreateTodoItemForm
+          className="mt-3"
+          setIsDialogOpen={setIsDialogOpen}
+          todo_id={todo_id}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateTodoItemDialog;
