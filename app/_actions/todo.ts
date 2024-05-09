660169c7b91ebfc841f9e36cd8250bbdc17d954f
Farhan <farhan@aththariq.com>
2024-05-09 09:21:19 +0700

    refactor: Remove unused import in todo actions

--- app/_actions/todo.ts
+++ app/_actions/todo.ts
@@ -1,7 +1,6 @@
 'use server';
 
 import { backendApi } from '@/lib/axios';
-import { getErrorMessage } from '@/lib/utils';
 import { createTodoSchema } from '@/lib/validators/todo';
 import { Todo } from '@/types';
 import { createSafeActionClient } from 'next-safe-action';
@@ -19,5 +18,5 @@ export const createTodo = action(createTodoSchema, async (values) => {
 export const getTodos = async () => {
   const { data } = await backendApi.get('/todos');
 
-  return data;
+  return data as Todo[];
 };
