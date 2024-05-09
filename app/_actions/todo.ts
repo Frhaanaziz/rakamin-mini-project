93d6a183d9f0e8fde45f0f02bede0c61953fc907
Farhan <farhan@aththariq.com>
2024-05-09 07:59:25 +0700

    feat: Add createTodo and getTodos functions to todo actions
    This commit adds the createTodo and getTodos functions to the todo actions in the todo.ts file. The createTodo function sends a POST request to the backend API to create a new todo, while the getTodos function sends a GET request to retrieve all todos. These functions will be used to interact with the backend API for todo-related operations in the application.

---
+++ app/_actions/todo.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { backendApi } from '@/lib/axios';
+import { getErrorMessage } from '@/lib/utils';
+import { createTodoSchema } from '@/lib/validators/todo';
+import { createSafeActionClient } from 'next-safe-action';
+
+export const action = createSafeActionClient();
+
+export const createTodo = action(createTodoSchema, async (values) => {
+  try {
+    const { data } = await backendApi.post('/todos', values);
+
+    return { error: null, data };
+  } catch (error) {
+    console.error('Error creating todo:', error);
+    return { error: getErrorMessage(error), data: null };
+  }
+});
+
+export const getTodos = async () => {
+  try {
+    const { data } = await backendApi.get('/todos');
+
+    return { error: null, data };
+  } catch (error) {
+    console.error('Error getting todos:', error);
+    return { error: getErrorMessage(error), data: null };
+  }
+};
