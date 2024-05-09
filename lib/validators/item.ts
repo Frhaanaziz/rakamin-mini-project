f16e1eec78e399e79ce1fb5c3d825032fbac447a
Farhan <farhan@aththariq.com>
2024-05-09 07:58:42 +0700

    refactor: Update item validation schema to include progress_percentage.

--- lib/validators/item.ts
+++ lib/validators/item.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { todoSchema } from './todo';
 
 export const itemSchema = z.object({
   id: z.number().int(),
@@ -6,23 +7,38 @@ export const itemSchema = z.object({
     .string()
     .trim()
     .min(1, { message: 'Name must be at least 1 character' }),
-  done: z.boolean().nullable(),
-  todo_id: z.number().int(),
-  created_at: z.coerce.date(),
-  updated_at: z.coerce.date(),
   progress_percentage: z
     .number()
     .min(0, { message: 'Progress percentage must be at least 0' })
     .max(100, { message: 'Progress percentage must be at most 100' }),
+  todo_id: z.number().int(),
+  done: z.boolean().nullable(),
+  created_at: z.coerce.date(),
+  updated_at: z.coerce.date(),
 });
 
-export const createItemSchema = itemSchema.pick({
-  name: true,
-  todo_id: true,
-});
+export const getItemsSchema = itemSchema.pick({ todo_id: true });
 
-export const updateItemSchema = itemSchema.omit({
+export const createItemSchema = itemSchema.omit({
   id: true,
+  done: true,
   created_at: true,
   updated_at: true,
 });
+
+export const updateItemSchema = itemSchema.omit({
+  created_at: true,
+  updated_at: true,
+});
+
+export const deleteItemSchema = itemSchema.pick({
+  id: true,
+  todo_id: true,
+});
+
+export const moveItemSchema = itemSchema
+  .pick({
+    todo_id: true,
+    id: true,
+  })
+  .extend({ target_todo_id: todoSchema.shape.id });
