d9a91bc320be452c8b16adaef4a8faf159f1d404
Farhan <farhan@aththariq.com>
2024-05-09 23:57:08 +0700

    refactor: Update Header component layout for better responsiveness

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -2,9 +2,11 @@ import CreateTodoDialog from '../dialogs/create-todo-dialog';
 
 const Header = () => {
   return (
-    <header className="flex gap-4 h-16 items-center px-6 border-b-2">
-      <h1 className="text-lg xs:text-xl font-bold">Product Roadmap</h1>
-      <CreateTodoDialog />
+    <header className="border-b-2">
+      <div className="container flex gap-4  h-16 items-center">
+        <h1 className="text-lg xs:text-xl font-bold">Product Roadmap</h1>
+        <CreateTodoDialog />
+      </div>
     </header>
   );
 };
