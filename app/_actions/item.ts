44421ade9ff5889158cc2e9d411a99cbe459abfa
Farhan <farhan@aththariq.com>
2024-05-09 20:26:38 +0700

    feat: Refactor item actions to use async/await syntax

--- app/_actions/item.ts
+++ app/_actions/item.ts
@@ -9,6 +9,7 @@ import {
   moveItemSchema,
   updateItemSchema,
 } from '@/lib/validators/item';
+import { Item } from '@/types';
 import { createSafeActionClient } from 'next-safe-action';
 import { revalidatePath } from 'next/cache';
 
@@ -17,70 +18,45 @@ export const action = createSafeActionClient();
 export const createTodoItem = action(createItemSchema, async (item) => {
   const { todo_id, ...rest } = item;
 
-  try {
-    const { data } = await backendApi.post(`/todos/${todo_id}/items`, rest);
+  const { data } = await backendApi.post(`/todos/${todo_id}/items`, rest);
 
-    revalidatePath('/');
-    return { error: null, data };
-  } catch (error) {
-    console.error('Error creating item:', error);
-    return { error: getErrorMessage(error), data: null };
-  }
+  revalidatePath('/');
+  return data;
 });
 
 export const getTodoItems = action(getItemsSchema, async (item) => {
-  try {
-    const { data } = await backendApi.get(`/todos/${item.todo_id}/items`);
+  const { data } = await backendApi.get(`/todos/${item.todo_id}/items`);
 
-    return { error: null, data };
-  } catch (error) {
-    console.error('Error getting items:', error);
-    return { error: getErrorMessage(error), data: null };
-  }
+  return data as Item[];
 });
 
 export const updateTodoItem = action(updateItemSchema, async (item) => {
-  try {
-    const { data } = await backendApi.patch(
-      `/todos/${item.todo_id}/items/${item.id}`,
-      item
-    );
+  const { data } = await backendApi.patch(
+    `/todos/${item.todo_id}/items/${item.id}`,
+    item
+  );
 
-    revalidatePath('/');
-    return { error: null, data };
-  } catch (error) {
-    console.error('Error updating item:', error);
-    return { error: getErrorMessage(error), data: null };
-  }
+  revalidatePath('/');
+  return data;
 });
 
 export const deleteTodoItem = action(deleteItemSchema, async (item) => {
-  try {
-    const { data } = await backendApi.delete(
-      `/todos/${item.todo_id}/items/${item.id}`
-    );
+  const { data } = await backendApi.delete(
+    `/todos/${item.todo_id}/items/${item.id}`
+  );
 
-    revalidatePath('/');
-    return { error: null, data };
-  } catch (error) {
-    console.error('Error deleting item:', error);
-    return { error: getErrorMessage(error), data: null };
-  }
+  revalidatePath('/');
+  return data;
 });
 
 export const moveTodoItem = action(moveItemSchema, async (item) => {
   const { todo_id, id, ...rest } = item;
 
-  try {
-    const { data } = await backendApi.patch(
-      `/todos/${todo_id}/items/${id}`,
-      rest
-    );
+  const { data } = await backendApi.patch(
+    `/todos/${todo_id}/items/${id}`,
+    rest
+  );
 
-    revalidatePath('/');
-    return { error: null, data };
-  } catch (error) {
-    console.error('Error moving item:', error);
-    return { error: getErrorMessage(error), data: null };
-  }
+  revalidatePath('/');
+  return data;
 });
