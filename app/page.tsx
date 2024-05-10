07e4521acb209da4f263d6c52b6dd655c9df7bd4
Farhan <farhan@aththariq.com>
2024-05-10 11:34:33 +0700

    feat: Update Home page to display todos with dynamic colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Todo from '@/components/todo';
 import { getTodos } from './_actions/todo';
+import { todoColorList } from '@/lib/constants';
 
 export default async function Home() {
   const { data: todos } = await getTodos(undefined);
@@ -9,11 +10,16 @@ export default async function Home() {
     <main className="container my-6">
       {/* <KanbanBoard /> */}
       <section className="grid grid-cols-4 gap-4">
-        {todos.map((todo) => (
-          <div key={todo.id}>
-            <Todo todo={todo} />
-          </div>
-        ))}
+        {todos.map((todo, i) => {
+          const colorIndex = i % todoColorList.length;
+          const color = todoColorList[colorIndex];
+
+          return (
+            <div key={todo.id}>
+              <Todo todo={todo} color={color} />
+            </div>
+          );
+        })}
       </section>
     </main>
   );
