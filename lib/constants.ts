e3eca89435b20de1aa4beab1c44455c60cb444f6
Farhan <farhan@aththariq.com>
2024-05-10 11:34:19 +0700

    feat: Add constants for todo colors

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const todoColorList = [
+  { lightColor: '#ECFEFF', deepColor: '#06B6D4' },
+  { lightColor: '#FEFCE8', deepColor: '#EAB308' },
+  { lightColor: '#FEF2F2', deepColor: '#F87171' },
+  { lightColor: '#ECFDF5', deepColor: '#10B981' },
+  { lightColor: '#EEF2FF', deepColor: '#818CF8' },
+  { lightColor: '#F5F3FF', deepColor: '#A78BFA' },
+] as const;
