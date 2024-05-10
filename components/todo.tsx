749f7d11e8b3e38f3e18ca73c410406f569b59ce
Farhan <farhan@aththariq.com>
2024-05-10 11:34:27 +0700

    refactor: Update Todo component to use dynamic colors for background and badge

--- components/todo.tsx
+++ components/todo.tsx
@@ -3,19 +3,33 @@ import TodoItem from './todo-item';
 import { Badge } from './ui/badge';
 import { getTodoItems } from '@/app/_actions/item';
 import CreateTodoItemDialog from './dialogs/create-todo-item-dialog';
+import { todoColorList } from '@/lib/constants';
 
 interface TodoProps {
   todo: TodoType;
+  color: (typeof todoColorList)[number];
 }
 
-const Todo = async ({ todo }: TodoProps) => {
+const Todo = async ({ todo, color }: TodoProps) => {
   const { id, title, description } = todo;
   const { data: todoItems } = await getTodoItems({ todo_id: id });
   if (!todoItems) throw new Error('No todo items found');
 
   return (
-    <div className={`bg-cyan-50/50 p-4 border border-cyan-500 rounded`}>
-      <Badge variant="outline" className={`text-cyan-500 border-cyan-500`}>
+    <div
+      className={`p-4 border rounded`}
+      style={{
+        backgroundColor: color.lightColor,
+        borderColor: color.deepColor,
+      }}
+    >
+      <Badge
+        variant="outline"
+        style={{
+          color: color.deepColor,
+          borderColor: color.deepColor,
+        }}
+      >
         {title}
       </Badge>
       <p className="my-2 text-xs font-bold">{description}</p>
