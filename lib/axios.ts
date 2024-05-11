f7f69c628982004c07150e7f4358d2dd58a667c4
Farhan <farhan@aththariq.com>
2024-05-11 17:38:29 +0700

    refactor: Update backendApi configuration in axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+/**
+ * Creates an instance of Axios with the specified configuration.
+ * @remarks
+ * The `backendApi` instance is configured with the base URL and headers for making HTTP requests to the backend API.
+ * The base URL is retrieved from the `API_URL` environment variable, and the authorization header is set using the `API_KEY` environment variable.
+ */
 export const backendApi = axios.create({
   baseURL: process.env.API_URL,
   headers: {
