540f6cce80bc4bd3f192477bbdc77c1ece66d614
Farhan <farhan@aththariq.com>
2024-05-11 17:38:17 +0700

    refactor: Add DraggableData type to index.ts

--- types/index.ts
+++ types/index.ts
@@ -4,3 +4,16 @@ import { z } from 'zod';
 
 export type Todo = z.infer<typeof todoSchema>;
 export type Item = z.infer<typeof itemSchema>;
+export type TodoWithItems = Todo & { items: Item[] };
+
+export interface ItemDragData {
+  type: 'Item';
+  item: Item;
+}
+
+export interface TodoDragData {
+  type: 'Todo';
+  todo: TodoWithItems;
+}
+
+export type DraggableData = ItemDragData | TodoDragData;
