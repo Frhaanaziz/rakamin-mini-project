2557501f138eeef14faf510c8eb191fe5b8d40db
Farhan <farhan@aththariq.com>
2024-05-11 17:40:29 +0700

    refactor: Update Home page to use TodoGroupSection component for rendering todos with items

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,31 @@
-import Todo from '@/components/todo';
+import TodoGroupSection from '@/components/todo-group-section';
 import { getTodos } from './_actions/todo';
-import { todoColorList } from '@/lib/constants';
+import { getTodoItems } from './_actions/item';
+import { Suspense } from 'react';
 
 export default async function Home() {
   const { data: todos } = await getTodos(undefined);
   if (!todos) throw new Error('No todos found');
 
+  const todosItems = await Promise.all(
+    todos.map(async (todo) => {
+      const { data: todoItems } = await getTodoItems({ todo_id: todo.id });
+      if (!todoItems) throw new Error('No todo items found');
+
+      return todoItems;
+    })
+  );
+
+  const todosWithItems = todos.map((todo, i) => {
+    return {
+      ...todo,
+      items: todosItems[i],
+    };
+  });
+
   return (
     <main className="container my-6">
-      {/* <KanbanBoard /> */}
-      <section className="grid grid-cols-4 gap-4">
-        {todos.map((todo, i) => {
-          const colorIndex = i % todoColorList.length;
-          const color = todoColorList[colorIndex];
-
-          return (
-            <div key={todo.id}>
-              <Todo todo={todo} color={color} />
-            </div>
-          );
-        })}
-      </section>
+      <TodoGroupSection todos={todosWithItems} />
     </main>
   );
 }
